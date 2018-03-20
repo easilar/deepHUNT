f84971f1aceacdd8d8bb268ff5631f47988b2967
easilar <easilar@gmail.com>
2018-03-20 10:54:20 +0100

    test for sdf reader

--- sdf_utils/python/helper.py
+++ sdf_utils/python/helper.py
@@ -1,8 +1,23 @@
 import os, sys
 
-def getNmolecules(sdffile, printOption=False):
+def readFile(sdffile,printOption=False):
 	cur_file = open(sdffile,'r')
-	count = cur_file.read().count('$$$$')
-	if printOption : print 'number of moleculus found in file' , sdffile, 'is' , count
+	readyfile = cur_file.read()
+	if printOption : print 'file is ready to be read'
+	return readyfile
+
+def getNmolecules(readyfile, printOption=False):
+	count = readyfile.count('$$$$')
+	if printOption : print 'number of moleculus :' , count
 	return count
 
+def getMolecules(readyfile, printOption=False):
+	molecules = readyfile.split('$$$$')
+	return molecules
+
+def getMoleculeFromIndex(molecules,index):
+	molecule = molecules[index]
+	return molecule
+
+#def createMoleculeDict(molecule):
+	
