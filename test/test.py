3e4f7542d3e2d364b9b602018dddf538a49f974e
easilar <easilar@gmail.com>
2018-03-20 11:22:12 +0100

    new mol sdf

--- test/test.py
+++ test/test.py
@@ -5,8 +5,9 @@ cur_f = 'sdf_utils/data/Eleni_mulliner_curated_w_ID.sdf'
 f1 = readFile(cur_f,printOption=False)
 Mols=getMolecules(f1)
 len(Mols)
-Mol = getMoleculeFromIndex(Mols,0)
-ftow = open('test/testmol.sdf','w')
+mol_index = 15
+Mol = getMoleculeFromIndex(Mols,mol_index)
+ftow = open('test/testmol'+str(mol_index)+'.sdf','w')
 ftow.write(Mol+'\n$$$$')
 ftow.close()
 
