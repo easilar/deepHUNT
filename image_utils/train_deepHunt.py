49180ffcb230b838c509a00eefbf7f36cc93c63a
easilar <easilar@gmail.com>
2018-04-05 15:08:18 +0200

    new metrics

--- image_utils/train_deepHunt.py
+++ image_utils/train_deepHunt.py
@@ -13,7 +13,7 @@ from keras.preprocessing.image import ImageDataGenerator , img_to_array
 from sklearn.model_selection import train_test_split
 from keras.utils import to_categorical
 from deepHunt import deepHunt
-from metrics_helper import evaluate as meval
+from metric_helper import precision , sensitivity, specificity, accuracy, bal_acc, fpr, fnr, fmeasure, mcc, youden, AUC, gmean
 from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
@@ -32,10 +32,16 @@ import configure as conf
 import h5py    
 
 f1 = h5py.File('hdf5/dataset.hdf5','r+')
-data , labels = f1['data'] , f1['labels']
+data , labels = f1['data'][()] , f1['labels'][()]
 print len(data) , len(labels)
+print type(data) , type(labels)
 
-(trainX, testX, trainY, testY) = train_test_split(data, labels, test_size=0.30, random_state=42)
+val_data = data[:28362]
+val_labels = labels[:28362]
+data_ = data[28362:]
+labels_ = labels[28362:]
+
+(trainX, testX, trainY, testY) = train_test_split(data_, labels_, test_size=0.30, random_state=42)
 
 # convert the labels from integers to vectors
 trainY = to_categorical(trainY, num_classes=2)
@@ -50,13 +56,16 @@ aug = ImageDataGenerator(rotation_range=30, width_shift_range=0.1,
 print("[INFO] compiling model...")
 model = deepHunt.build(width=conf.sizeX, height=conf.sizeY, depth=3, classes=2)
 opt = Adam(lr=conf.INIT_LR, decay=conf.INIT_LR / conf.EPOCHS) #optimizer
+
 model.compile(loss="binary_crossentropy", optimizer=opt,
 	metrics=["accuracy",metrics.mae,metrics.categorical_accuracy,'binary_accuracy'])
 
 
 #tbCallBack = callbacks.TensorBoard(log_dir='logs/log_noPh, histogram_freq=0,  
 #          write_graph=True, write_images=True)
-tensorboard = TensorBoard(log_dir="logs/{}".format(time.time()))
+tensorboard = TensorBoard(log_dir="logs/{}".format(time.time()),histogram_freq=1,write_graph=True,)
+tensorboard.set_model(model)
+
 # train the network
 print("[INFO] training network...")
 #H = model.fit_generator(aug.flow(trainX, trainY, batch_size=conf.BS),
